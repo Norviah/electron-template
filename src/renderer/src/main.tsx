00035d5b139843e56ca21225f295606248af1499
Norviah <21983700+Norviah@users.noreply.github.com>
2024-10-12 02:20:19 -0700

    feat(frontend): add tanstack-router

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,11 +1,71 @@
-import './assets/main.css'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  Outlet,
+  RouterProvider,
+  Link,
+  createRouter,
+  createRoute,
+  createRootRoute,
+} from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import './base.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <div className='p-2 flex gap-2'>
+        <Link to='/' className='[&.active]:font-bold'>
+          Home
+        </Link>{' '}
+        <Link to='/about' className='[&.active]:font-bold text-primary'>
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: function Index() {
+    return (
+      <div className='p-2'>
+        <h3>Welcome Home!</h3>
+      </div>
+    );
+  },
+});
+
+const aboutRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/about',
+  component: function About() {
+    return <div className='p-2'>Hello from About!</div>;
+  },
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById('root')!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+}
