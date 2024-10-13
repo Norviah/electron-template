8800790014d636ebe22e9b514b03e0b7dce0417d
Norviah <21983700+Norviah@users.noreply.github.com>
2024-10-12 17:00:24 -0700

    feat(frontend): add `/demo` route

--- src/renderer/routes/__root.tsx
+++ src/renderer/routes/__root.tsx
@@ -11,6 +11,9 @@ export const Route = createRootRoute({
         <Link to='/about' className='[&.active]:font-bold'>
           About
         </Link>
+        <Link to='/demo' className='[&.active]:font-bold'>
+          Demo
+        </Link>
       </div>
       <hr />
       <Outlet />
