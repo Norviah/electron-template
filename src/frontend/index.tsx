931c5a50df2c08c313063c74ce8e16174c826a0b
Norviah <21983700+Norviah@users.noreply.github.com>
2024-10-13 16:22:19 -0700

    feat(frontend): add toast provider for toast notifications

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider, createHashHistory, createRouter } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import { ThemeProvider } from './components/ThemeProvider';
+import { ToastProvider } from './components/ToastProvider';
 
 import { queryClient, t, trpcClient } from '@shared/trpc/config';
 import { routeTree } from './routeTree.gen';
@@ -33,6 +34,8 @@ if (!rootElement.innerHTML) {
           </QueryClientProvider>
         </t.Provider>
       </ThemeProvider>
+
+      <ToastProvider />
     </StrictMode>,
   );
 }
