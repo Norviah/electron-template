9ec019ef32833fdb4fb90fe6ab250e664b6247e3
Norviah <21983700+Norviah@users.noreply.github.com>
2024-10-13 17:16:42 -0700

    feat(frontend): wrap all components in a scrollable area

--- src/frontend/routes/__root.tsx
+++ src/frontend/routes/__root.tsx
@@ -1,31 +1,34 @@
 import { Link } from '@frontend/components/Link';
 import { TitleBar } from '@frontend/components/TitleBar';
+import { ScrollArea } from '@frontend/components/ui/ScrollArea';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <div className='flex h-screen flex-col text-foreground-light'>
       <TitleBar />
 
-      <div className='container space-y-10 pt-14 py-5 text-foreground-light'>
-        <div className='space-x-2'>
-          <Link to='/' className='[&.active]:text-foreground'>
-            Home
-          </Link>
+      <ScrollArea className='container flex-grow'>
+        <div className='space-y-5'>
+          <div className='space-x-2'>
+            <Link to='/' className='[&.active]:text-foreground'>
+              Home
+            </Link>
 
-          <Link to='/about' className='[&.active]:text-foreground'>
-            About
-          </Link>
+            <Link to='/about' className='[&.active]:text-foreground'>
+              About
+            </Link>
 
-          <Link to='/demo' className='[&.active]:text-foreground'>
-            Demo
-          </Link>
+            <Link to='/demo' className='[&.active]:text-foreground'>
+              Demo
+            </Link>
+          </div>
+
+          <Outlet />
+          <TanStackRouterDevtools />
         </div>
-
-        <Outlet />
-        <TanStackRouterDevtools />
-      </div>
-    </>
+      </ScrollArea>
+    </div>
   ),
 });
