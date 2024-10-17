5b65ecaa7009251ff2e1f665c232120b90486352
Norviah <21983700+Norviah@users.noreply.github.com>
2024-10-16 17:13:22 -0700

    chore: move asset files into `assets/`

--- src/shared/lib/paths.ts
+++ src/shared/lib/paths.ts
@@ -1,7 +1,9 @@
 import { path as ROOT } from 'app-root-path';
+import { app } from 'electron';
 import { join } from 'node:path';
 
 export { ROOT };
 
-export const RESOURCES: string = join(ROOT, 'resources');
-export const ICON: string = join(RESOURCES, 'icon.png');
+export const RESOURCES: string = app.isPackaged ? join(ROOT, '..', 'assets') : join(ROOT, 'assets');
+export const ICON: string = join(RESOURCES, 'icon.ico');
+export const ICON_PNG: string = join(RESOURCES, 'icon.png');
